e679c82ab4726204710d471f3285cbc7bf9e795c
Varokas Panusuwan <varokas@panusuwan.com>
2013-09-28 21:43:04 -0700

    add base cases

--- src/main/java/com/huskycode/dojo/tdd/lesson1/Grade.java
+++ src/main/java/com/huskycode/dojo/tdd/lesson1/Grade.java
@@ -1,4 +1,11 @@
 package com.huskycode.dojo.tdd.lesson1;
 
 public class Grade {
+    public int compute(int score) {
+        if (score >= 80) return 4;
+        if (score >= 70) return 3;
+        if (score >= 60) return 2;
+        if (score >= 50) return 1;
+        return 0;
+    }
 }
--- src/test/java/com/huskycode/dojo/tdd/lesson1/GradeTest.java
+++ src/test/java/com/huskycode/dojo/tdd/lesson1/GradeTest.java
@@ -2,9 +2,38 @@ package com.huskycode.dojo.tdd.lesson1;
 
 import org.junit.Test;
 
-public class GradeTest {
-    @Test
-    public void test() {
+import static org.junit.Assert.assertEquals;
 
+public class GradeTest {
+
+    private Grade grade = new Grade();
+
+    @Test
+    public void whenScoreIs80OrMore_gradeIs4() {
+        assertEquals(4, grade.compute(81));
+        assertEquals(4, grade.compute(80));
+    }
+
+    @Test
+    public void whenScoreIs70OrMore_gradeIs3() {
+        assertEquals(3, grade.compute(71));
+        assertEquals(3, grade.compute(70));
+    }
+
+    @Test
+    public void whenScoreIs60OrMore_gradeIs2() {
+        assertEquals(2, grade.compute(61));
+        assertEquals(2, grade.compute(60));
+    }
+
+    @Test
+    public void whenScoreIs50OrMore_gradeIs1() {
+        assertEquals(1, grade.compute(51));
+        assertEquals(1, grade.compute(50));
+    }
+
+    @Test
+    public void whenScoreIsLessThan50_gradeIs0() {
+        assertEquals(0, grade.compute(49));
     }
 }
